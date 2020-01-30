5a0ac55431fcf3b114d196939f1b4103cb193f7d
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-30 17:06:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(value)
  if value <= 1 || value == 0 || value == 1
     return false
   elsif
-(2..Math.sqrt -1).each do |n|
+(2..value -1).each do |n|
    return false if num % n == 0
   end
 end
