91be37502c7cdb71f5a2ec0f94b28f32cf2d414f
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-30 17:02:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,9 @@ def prime?(n)
   if value <= 1 
     return false
   elsif 
+  elsif n <= 3
+    return true
 (2..Math.sqrt(num)).each do |n|
    return false if num % n == 0
   end
-end
-end
-  true
 end
